48acf17e862aac3c1961693f6c600a9f09c38239
Yelyzaveta Dymchenko <yelyzaveta.dymchenko@zippie.com>
2024-11-04 09:52:27 +0000

    feat: add entrypoint.sh which add secret_key to a configuration file

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+CONFIG_FILE="config.toml"
+
+if [ -n "$SECRET_KEY" ]; then
+  if [ -s "$CONFIG_FILE" ]; then
+    echo "" >> "$CONFIG_FILE"
+  fi
+  echo "secret_key = \"$SECRET_KEY\"" >> "$CONFIG_FILE"
+fi
+
+/cartesi-coprocessor-solver/cartesi-coprocessor-solver
